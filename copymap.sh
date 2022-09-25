09a8b10b1187cd7808a2eaef06ba1573b72ccb80
the-root42 <adam@42roots.com>
2022-09-25 13:29:06 +0000

    update map

--- copymap.sh
+++ copymap.sh
@@ -1,4 +1,6 @@
-cp ~/.config/Epic/FactoryGame/Saved/SaveGames/server/42roots_2.0_autosave_0.sav ./
+/home/steam/.config/Epic/FactoryGame/Saved/SaveGames/server/42roots_2.0_autosave_0.sav /home/steam/gitfiles/
+git add copymap.sh
+git add 42roots_2.0_autosave_0.sav 
 git commit -m "update map"
 git branch -M main
 git push -u origin main
